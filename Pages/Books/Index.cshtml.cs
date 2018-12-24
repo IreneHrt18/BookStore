3d62feef86b754681d489c1e38f1522645bca7cc
臣骏 刘 <theliuchenjun@icloud.com>
2018-12-24 08:28:02 +0800

    合并主分支

--- Pages/Books/Index.cshtml.cs
+++ Pages/Books/Index.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using BookStore.Models;
+using ContosoUniversity;
 
 namespace BookStore.Pages.Books
 {
@@ -18,21 +19,33 @@ namespace BookStore.Pages.Books
             _context = context;
         }
 
-        public IList<Book> Book { get;set; }
+      //  public IList<Book> Book { get;set; }
+        public PaginatedList<Book> Book { get; set; }
+        public IQueryable<Book> Books { get; set; }
 
         [BindProperty(SupportsGet =true)]
         public string searchString { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(int ? pageIndex)
         {
-
             if (!String.IsNullOrEmpty(searchString))
             {
-                Book = await _context.Book.Where(item => item.BookName.Contains(searchString)).ToListAsync();
+                // Book = await _context.Book.Where(item => item.BookName.Contains(searchString)).ToListAsync();
+                Books = _context.Book.Where(item => item.BookName.Contains(searchString));
+                int pageSize = 3;
+                Book = await PaginatedList<Book>.CreateAsync(Books.AsNoTracking(), pageIndex ?? 1, pageSize);
             }
             else
             {
-                Book = await _context.Book.ToListAsync();
+                //  Book = await _context.Book.ToListAsync();
+                Books = from r in _context.Book
+                        select r;
+                // Books.OrderByDescending(o => o.Price);
+
+
+                int pageSize = 3;
+                Book = await PaginatedList<Book>.CreateAsync(
+                    Books.AsNoTracking(), pageIndex ?? 1, pageSize);
             }
         }
         //添加购物车
