c0bb82a17ecd5277786e76c485c43b30648d2684
xiaoshuaifei <1224970003@qq.com>
2018-12-25 11:48:17 +0800

    完成order的分页

--- Pages/Orders/Index.cshtml.cs
+++ Pages/Orders/Index.cshtml.cs
@@ -22,18 +22,27 @@ namespace BookStore.Pages.Orders
          [BindProperty(SupportsGet =true)]
         public string SearchString { get; set; }
 
-        public IList<Order> Order { get;set; }
+       // public IList<Order> Order { get;set; }
 
-        
+        public IQueryable<Order> OrderQuery { get; set; }
+        public PaginatedList<Order> OrderList { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(int ?pageIndex)
         {
-            
+            OrderQuery = from r in _context.Order
+                         select r;
+            OrderQuery.Include(o => o.User)
+                .Where(o => o.User.Id == 1002)
+                .Include(o => o.OrderBooks);
+                       
 
-            Order = await _context.Order
-                    .Include(o => o.User)
-                    .Where(o => o.User.Id == 1002)
-                    .Include(o => o.OrderBooks).ToListAsync();
+            //Order = await _context.Order
+            //        .Include(o => o.User)
+            //        .Where(o => o.User.Id == 1002)
+            //        .Include(o => o.OrderBooks).ToListAsync();
+            int pageSize = 3;
+            OrderList = await PaginatedList<Order>.CreateAsync(
+                OrderQuery.AsNoTracking(), pageIndex ?? 1, pageSize);
 
         }
     }
